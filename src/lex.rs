2707dcd06cdfcd19a154c93b950f45fceaa9c0bc
chrisdasht4 <christofish2@gmail.com>
2022-04-07 11:46:20 -0400

    Implemented command line interface; added word and attribute structs and respective parsers.

---
+++ src/lex.rs
@@ -0,0 +1,134 @@
+use std::{cmp::Ordering,io::Result,io::Error,collections::hash_set::HashSet};
+use serde_json::Value;
+
+fn cuo(s : &str) -> Error {
+    Error::new(std::io::ErrorKind::Other,s)
+}
+
+#[derive(Hash,PartialEq,Eq)]
+pub enum Wordclass { N, V, M, P, }
+
+#[derive(PartialEq,Eq)]
+pub struct Attribute {
+    name: String,
+    form: String,
+    place: Ordering,
+    affects: HashSet<Wordclass>,
+}
+
+pub trait Affect {
+    fn can_affect(&self, c : Wordclass) -> bool;
+}
+
+impl Affect for Attribute {
+    fn can_affect(&self, c : Wordclass) -> bool {
+        self.affects.contains(&c)
+    }
+}
+
+pub enum Word<Attr> {
+    Noun(String,String,Vec<Attr>),
+    Verb(String,String,Vec<Attr>),
+    Modifier(String,String,Vec<Attr>),
+    Particle(String,String,Vec<Attr>),
+}
+
+pub fn add_attr<Attr : Eq+Affect>(w : Word<Attr>, a : Attr) -> Word<Attr> {
+    match w {
+        Word::Noun(root,gloss,mut xs) => {
+            if a.can_affect(Wordclass::N) { xs.push(a); }
+            Word::Noun(root,gloss,xs)
+        },
+        Word::Verb(root,gloss,mut xs) => {
+            if a.can_affect(Wordclass::V) { xs.push(a); }
+            Word::Noun(root,gloss,xs)
+        },
+        Word::Modifier(root,gloss,mut xs) => {
+            if a.can_affect(Wordclass::M) { xs.push(a); }
+            Word::Modifier(root,gloss,xs)
+        },
+        Word::Particle(root,gloss,mut xs) => {
+            if a.can_affect(Wordclass::P) { xs.push(a); }
+            Word::Particle(root,gloss,xs)
+        },
+    }
+}
+
+pub fn get_word<'a>(s : &'a String, json : &Value) -> Result<Word<Attribute>> {
+    let root = String::from(s);
+    let wordinfo = &json["vocab"][s.clone()];
+    match wordinfo {
+        Value::Null => {return Err(cuo("Word not found!"));}
+        _ => ()
+    }
+    let gloss = String::from(Option::unwrap(wordinfo["gloss"].as_str()));
+    match wordinfo["class"].as_str() {
+        Some("N") => Ok(Word::Noun(root,gloss,Vec::new())),
+        Some("V") => Ok(Word::Verb(root,gloss,Vec::new())),
+        Some("M") => Ok(Word::Modifier(root,gloss,Vec::new())),
+        Some("P") => Ok(Word::Particle(root,gloss,Vec::new())),
+        _ => Err(cuo("Unrecognized word class!"))
+    }
+}
+
+pub fn get_attr(name : String, json : &Value) -> Result<Attribute> {
+    let attrinfo = &json["attributes"][name.clone()];
+    match attrinfo {
+        Value::Null => {return Err(cuo("Word not found!"));}
+        _ => ()
+    }
+    let form = String::from(Option::unwrap(attrinfo["form"].as_str()));
+    let place = Option::unwrap(attrinfo["pos"].as_i64()).cmp(&0);
+    let mut affects = HashSet::new();
+    match &attrinfo["affects"] {
+        Value::Array(cs) => {
+            for c in cs {
+                match c.as_str() {
+                    Some("N") => { affects.insert(Wordclass::N); },
+                    Some("V") => { affects.insert(Wordclass::V); },
+                    Some("M") => { affects.insert(Wordclass::M); },
+                    Some("P") => { affects.insert(Wordclass::P); },
+                    _ => { return Err(cuo("Unrecognized word class!")); }
+                }
+            }
+        }
+        _ => {return Err(cuo("Malformed JSON!"));}
+    }
+    Ok(Attribute {name,form,place,affects})
+}
+
+pub fn inflect(w : &Word<Attribute>) -> String {
+    let (root,xs) = match &w {
+        Word::Noun(root,_,xs) => (root,xs),
+        Word::Verb(root,_,xs) => (root,xs),
+        Word::Modifier(root,_,xs) => (root,xs),
+        Word::Particle(root,_,xs) => (root,xs),
+    };
+    let mut out = String::from(root);
+    for attr in xs {
+        match attr.place {
+            Ordering::Greater => out = out + &attr.form.clone(),
+            Ordering::Less => out = attr.form.clone() + &out,
+            Ordering::Equal => ()
+        }
+    }
+    out
+}
+
+pub fn gloss(w : &Word<Attribute>) -> String {
+    let (gloss,xs) = match &w {
+        Word::Noun(_,gloss,xs) => (gloss,xs),
+        Word::Verb(_,gloss,xs) => (gloss,xs),
+        Word::Modifier(_,gloss,xs) => (gloss,xs),
+        Word::Particle(_,gloss,xs) => (gloss,xs),
+    };
+    let mut out = String::from(gloss);
+    for attr in xs {
+        match attr.place {
+            Ordering::Greater => out = out + "-" + &attr.name.clone(),
+            Ordering::Less => out = attr.name.clone() + "-" + &out,
+            Ordering::Equal => ()
+        }
+    }
+    out
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,57 @@
 use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
-use std::{io::Result, io::prelude::*, io::BufReader, fs::File};
+use std::{io::Result, io::prelude::*, io::BufReader, fs::File, path::PathBuf,option::Option};
 use arboard::Clipboard;
+use serde_json::Value;
+use clap::Parser;
+mod lex;
 
-fn do_thing(frame : &mut Frame) {
-    let f_ = File::open("sample.txt");
-    match f_ {
-      Err(_) => { frame.set_label("We do a little trolling."); },
-      Ok(f) => {
-        let mut reader = BufReader::new(f);
-        let mut buffer = String::new();
-        match reader.read_line(&mut buffer) {
-          Err(_) => { frame.set_label("We do a little trolling."); },
-          Ok(_) => {
-            frame.set_label(&buffer);
-            let mut clipboard = Clipboard::new().unwrap();
-            clipboard.set_text(buffer.into()).unwrap();
-          }
-        }
-      }
-    };
+#[derive(Parser)]
+struct Cli {
+  #[clap(short,long)]
+  graphical: bool,
+  #[clap(parse(from_os_str))]
+  path: PathBuf,
+  pattern: Vec<String>,
+}
+
+fn do_thing(frame : &mut Frame) -> Result<()> {
+    let f = File::open("sample.txt")?;
+    let mut reader = BufReader::new(f);
+    let mut buffer = String::new();
+    let _ = reader.read_line(&mut buffer)?;
+    frame.set_label(&buffer);
+    let mut clipboard = Clipboard::new().unwrap();
+    clipboard.set_text(buffer.into()).unwrap();
+    let f2 = File::open("out.json")?;
+    let reader2 = BufReader::new(f2);
+    let v : Value = serde_json::from_reader(reader2)?;
+    println!("{}",v[0]["word"]);
+    Ok(())
 }
 
 fn main() -> Result<()> {
+    let args = Cli::parse();
+
+    if !args.graphical {
+        let mut ws : Vec<lex::Word<_>> = Vec::new();
+        let f2 = File::open(args.path)?;
+        let reader2 = BufReader::new(f2);
+        let json : Value = serde_json::from_reader(reader2)?;
+        for s in &args.pattern {
+            let mut data = s.split("+");
+            let root = String::from(Option::unwrap(data.next()));
+            let xs : Vec<String> = data.map(String::from).collect();
+            let mut w = lex::get_word(&root,&json)?;
+            for x in xs {
+                let a = lex::get_attr(x,&json)?;
+                w = lex::add_attr(w,a);
+            }
+            ws.push(w);
+        }
+        println!("{} {}",lex::inflect(&ws[0]),lex::inflect(&ws[1]));
+        println!("{} {}",lex::gloss(&ws[0]),lex::gloss(&ws[1]));
+        return Ok(())
+    }
     let app = app::App::default();
     let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
     let mut frame = Frame::new(0, 0, 400, 200, "");
@@ -29,7 +59,12 @@ fn main() -> Result<()> {
     wind.end();
 
     wind.show();
-    but.set_callback(move |_| { do_thing(&mut frame) });
+    but.set_callback(move |_| {
+        match do_thing(&mut frame) {
+          Err(_) => { frame.set_label("We do a little trolling."); },
+          Ok(_) => (),
+        }
+    });
     app.run().unwrap();
     Ok(())
 }
